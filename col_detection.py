9fe5894c23d3781d2483585eb92fe232ade0b3f5
devDesu <cuzmenko.tosha@gmail.com>
2015-02-26 23:31:31 +0300

    practically finished GUI

--- col_detection.py
+++ col_detection.py
@@ -2,69 +2,20 @@ import cv2
 import numpy as np
 from Tkinter import *
 import tkFileDialog
+import os.path
 
-lBound = np.array([0, 0, 0], dtype=np.uint8)    #b,g,r
-uBound = np.array([62,62,62], dtype=np.uint8)
+lBoun = np.array([0, 0, 0], dtype=np.uint8)    #b,g,r
+uBoun = np.array([62,62,62], dtype=np.uint8)
 
-def setBounds(ev):
-    case = str(ev.widget)[1:].split(".")
+def getPics(lBound, uBound, toSave, lHeight, uHeight, filename):
     try:
-        if case[1] == 'blueLower':
-            uBound[0] = ev.widget.get()
-        elif case[1] == 'greenLower':
-            uBound[1] = ev.widget.get()
-        elif case[1] == 'redLower':
-            lBound[2] = ev.widget.get()
-        elif case[1] == 'blueUpper':
-            uBound[0] = ev.widget.get()
-        elif case[1] == 'greenUpper':
-            uBound[1] = ev.widget.get()
-        elif case[1] == 'redUpper':
-            uBound[2] = ev.widget.get()
+        os.mkdir("black")
     except:
-        print "error"
-
-def selectFile(ev): 
-    fn = tkFileDialog.Open(root, filetypes = [('*.txt files', '.txt')]).show()
-    if fn == '':
-        return
-    flName.set(fn)
-  
-def createScales(seq, frame):
-    for nm in seq:
-        scale1 = Scale(frame,orient = HORIZONTAL,length = 400,
-                       from_ = 0, to = 255, tickinterval = 15, resolution = 1, name = nm[0])
-        scale1.set(nm[1])
-        label1 = Label(frame, text = nm[0])
-        label1.pack()
-        scale1.pack()
-        scale1.bind("<1>", setBounds)
-
-root=Tk()
-flName = StringVar(value="")
-label1 = Label(root, textvariable = flName)
-label1.pack()
-
-
-frame = Frame(root, width = 900, height = 200, name = "main")
-frame.pack(side = "top")
-bt = Button(frame, text = "Select file")
-bt.bind("<1>", selectFile)
-bt.pack()
-frame = Frame(root, width = 450, name="scalesLower")
-frame.pack(side="left")
-createScales([['blueLower',0],['greenLower',0],['redLower',0]], frame)
-frame = Frame(root, width = 450, name = "scalesUpper")
-frame.pack(side="right")
-createScales([['blueUpper',0],['greenUpper',0],['redUpper',0]], frame)
-root.mainloop()
-
-
-def get_pics(lBound, uBound, toSave, lHeight, uHeight, filename):
+        pass
     saveable=False
     #lBound = np.array([0, 0, 0], dtype=np.uint8)
     #uBound = np.array([62,62,62], dtype=np.uint8)
-    cap = cv2.VideoCapture(filename)
+    cap = cv2.VideoCapture(filename.encode("utf-8"))
     i=0
     cnt=0
     try:
@@ -95,10 +46,83 @@ def get_pics(lBound, uBound, toSave, lHeight, uHeight, filename):
                     saveable=True
             cnt+=1
             if(saveable and cnt>=toSave):
-                    cv2.imwrite('file'+str(i)+'.png',mask)
+                    cv2.imwrite(os.path.join("black", ('file'+str(i)+'.png')),mask)
                     i+=1
                     cnt=0
             saveable=False
     except TypeError:
         print "Finishing..."
         cap.release()
+
+def setBounds(ev):
+    case = str(ev.widget)[1:].split(".")
+    try:
+        if case[1] == 'blueLower':
+            uBoun[0] = ev.widget.get()
+        elif case[1] == 'greenLower':
+            uBoun[1] = ev.widget.get()
+        elif case[1] == 'redLower':
+            lBoun[2] = ev.widget.get()
+        elif case[1] == 'blueUpper':
+            uBoun[0] = ev.widget.get()
+        elif case[1] == 'greenUpper':
+            uBoun[1] = ev.widget.get()
+        elif case[1] == 'redUpper':
+            uBoun[2] = ev.widget.get()
+    except:
+        print "error"
+
+def selectFile(ev): 
+    fn = tkFileDialog.Open(root, filetypes = [('video files', '.mp4 .mov .avi')]).show()
+    if fn == '':
+        return
+    flName.set(fn)
+  
+def createScales(seq, frame):
+    for nm in seq:
+        scale1 = Scale(frame, orient = HORIZONTAL, length = 400,
+                       from_ = 0, to = 255, tickinterval = 15, resolution = 1, name = nm[0])
+        scale1.set(nm[1])
+        label1 = Label(frame, text = nm[0])
+        label1.pack()
+        scale1.pack()
+        scale1.bind("<1>", setBounds)
+
+def tryProcess(ev):
+    getPics(lBoun, uBoun, int(text.get()), 35, 150, os.path.realpath(flName.get()))
+
+#main interface
+root=Tk()
+flName = StringVar(value="")
+frame = Frame(root, width = 900, height = 400, border = 5)
+frame.pack(side = "top", expand = True, fill = X)
+label1 = Label(frame, textvariable = flName)
+label1.pack()
+frame = Frame(root, width = 900, height = 400, border = 8)
+frame.pack(side = "top", expand = True, fill = X)
+bt = Button(frame, text = "Select file", width = 10)
+bt.bind("<1>", selectFile)
+bt.pack()
+frame = Frame(root, width = 900, height = 400, border = 8)
+frame.pack(side = "top", expand = True, fill = X)
+bt = Button(frame, text = "Go!", width = 10)
+bt.bind("<1>", tryProcess)
+bt.pack()#(side = "right")
+frame = Frame(root, width = 900, height = 100, border = 10)
+frame.pack(expand = True)
+#advanced settings
+label1 = Label(frame, text = "advanced", font = ("Times", "18"))
+label1.pack(fill = X)
+frame = Frame(root, width = 900, height = 100, border = 10)
+frame.pack(expand = True)
+text = Entry(frame)
+text.pack()
+label1 = Label(frame, text = "each %number% frame pic will be taken")
+label1.pack()
+frame = Frame(root, width = 450, name="scalesLower")
+frame.pack(side="left")
+createScales([['blueLower',0],['greenLower',0],['redLower',0]], frame)
+frame = Frame(root, width = 450, name = "scalesUpper")
+frame.pack(side="right")
+createScales([['blueUpper',62],['greenUpper',62],['redUpper',62]], frame)
+root.mainloop()
